1712298e057691e173e848595db6a9412f8ff090
674834420@qq.com <674834420@qq.com>
2016-11-16 20:53:19 +0800

    实现根据url打印pdf给前端输出

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
+var exec = require('child_process').exec;
+var util=require('util');
+var uuid=require('node-uuid');
+var path = require('path');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/printpdf', function(req, res, next) {
+  var pdfUrl = req.query.url;
+  var pdfpath = util.format('temp/%s.pdf', uuid.v4());
+  var rasterize= 'bin/rasterize.js';
+  //var _path=util.format("phantomjs %s %s %s A4", rasterize,pdfUrl, pdfpath);
+  var _path=util.format("phantomjs %s %s %s 800*600px", rasterize,pdfUrl, pdfpath);
+  exec(_path, function (error, stdout, stderr) {
+    if (error || stderr) {
+      console.log(error);
+      res.send(500, error || stderr);
+      return;
+    }
+    res.set('Content-Type', 'application/pdf');
+    res.download(pdfpath);
+  });
+
+});
+
 
 module.exports = router;
