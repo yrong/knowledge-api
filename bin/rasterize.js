cb9af492ab79e76422b8852303f127a79288d80b
674834420@qq.com <674834420@qq.com>
2016-11-18 13:15:03 +0800

    实现以phantomjs,casperjs的爬虫策略，进行文章打印

--- bin/rasterize.js
+++ bin/rasterize.js
@@ -2,7 +2,6 @@
 var page = require('webpage').create(),
     system = require('system'),
     address, output, size;
-
 if (system.args.length < 3 || system.args.length > 5) {
     console.log('Usage: rasterize.js URL filename [paperwidth*paperheight|paperformat] [zoom]');
     console.log('  paper (pdf output) examples: "5in*7.5in", "10cm*20cm", "A4", "Letter"');
@@ -10,8 +9,10 @@ if (system.args.length < 3 || system.args.length > 5) {
     console.log('                                   "800px*600px" window, clipped to 800x600');
     phantom.exit(1);
 } else {
-    address = system.args[1];
-    output = system.args[2];
+    //address = system.args[1];
+    address = "http://111.203.239.111:3001/#!/articles/41eeeb6a-fda7-4c11-8600-591a523fa557?mode=read";
+    //output = system.args[2];
+    output = "e:/temp/asdf.pdf";
     page.viewportSize = { width: 600, height: 600 };
     if (system.args.length > 3 && system.args[2].substr(-4) === ".pdf") {
         size = system.args[3].split('*');
@@ -42,6 +43,7 @@ if (system.args.length < 3 || system.args.length > 5) {
         } else {
             window.setTimeout(function () {
                 page.render(output);
+                console.log(page.content);
                 phantom.exit();
             }, 200);
         }
