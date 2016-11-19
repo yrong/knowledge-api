27dddaa5cd916a98fba2962b08d73d7c0a840019
674834420@qq.com <674834420@qq.com>
2016-11-19 23:44:12 +0800

    新增用户设置后台代码（暂未入库）

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var User=require('../sql/User');
+var fs = require('fs');
+var path = require('path');
+var formidable = require('formidable');
 var router = express.Router();
 
 /* GET users listing. */
@@ -20,6 +23,22 @@ router.put('/changepwd', function(req, res, next) {
       res.send({status:'ok'});
   });
 });
+//上传头像，设置分页等提交的form表单
+router.post('/userinfo', function(req, res, next) {
+  var form = new formidable.IncomingForm();
+  form.parse(req, function(error, fields, files) {
+    console.log("parsing done");
+    console.log(fields);
+    console.log(files.headimage.path);
+    //解析其他字段
+    var userid=fields.userid;
+    var per_page=fields.per_page;
+    var headimagepath=path.normalize('upload/'+userid+'.png');
+    fs.renameSync(files.headimage.path, headimagepath); //上传的图片另存为
+    //插入数据库记录
+    res.send({status:'ok'});
 
+  });
+});
 
 module.exports = router;
