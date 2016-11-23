197ce25f67ccb607616a44a49162a0c7ba75202c
674834420@qq.com <674834420@qq.com>
2016-11-23 20:36:58 +0800

    实现it_services的 树形级联修改，级联删除，级联查询

--- routes/it_services.js
+++ routes/it_services.js
@@ -11,12 +11,12 @@ var config=new Config();
 var pg_config=config.PG_Connection;//pg的连接参数
 var user=new User();
 var sql_template=new SQL_Template();
-//文章更新
+//it_services新增
 router.post('/', function(req, res, next) {
     //获取文章类型
     var token=req.body.token;
     var it_services=new IT_Services(req.body);
-       var client = new Client(pg_config);
+    var client = new Client(pg_config);
     try{
         client.connect();
     }
@@ -81,18 +81,18 @@ router.post('/', function(req, res, next) {
         client.end();
     })
 });
-//根据id删除
-router.delete('/:id', function(req, res, next) {
+//根据idcode删除。会级联删除，注意！
+router.delete('/:idcode', function(req, res, next) {
     //获取文章类型
-    var id=req.params.id;
-    if(id==undefined){
+    var idcode=req.params.idcode;
+    if(idcode==undefined){
         res.send({status:'未指定删除id，删除失败！'});
         return;
     }
     var token=req.body.token;
     async.waterfall([
             function (done) {
-                tokens.token_validate(token,function(info){
+                user.token_validate(token,function(info){
                     if(!info)
                         done('token验证失败，无权限更改数据！',null);
                     else
@@ -102,7 +102,7 @@ router.delete('/:id', function(req, res, next) {
             function (result, done) {
                 var client = new Client(pg_config);
                 client.connect();
-                let query = client.query('delete from template_article where id=$1', [id],function(err, result){
+                let query = client.query('delete from it_services where id in (select id from it_services a,(select path from it_services where idcode=$1) b where a.path<@b.path)', [idcode],function(err, result){
                     if(err)
                         done(err,null);
                     else
@@ -118,26 +118,30 @@ router.delete('/:id', function(req, res, next) {
         }
     )
 });
-//根据id更新若干字段
-router.put('/:id', function(req, res, next) {
-    //获取文章类型
-    var id=req.params.id;
+//根据idcode更新若干字段
+router.put('/:idcode', function(req, res, next) {
+    var idcode=req.params.idcode;
     var token=req.body.token;
-    if(id==undefined){
-        res.send({status:'未指定更新id，更新失败！'});
+    if(idcode==undefined){
+        res.send({status:'未指定更新idcode，更新失败！'});
+        return;
+    }
+    var it_services=new IT_Services(req.body);
+    if(it_services.name==undefined&&it_services.dependency==undefined){
+        res.send({status:'未指定更新字段数据，更新失败！'});
         return;
     }
     var client = new Client(pg_config);
-    try {
+    try{
         client.connect();
     }
     catch(e){
-        res.send({status:e});
+        res.send({status:'数据库连接错误'});
         return;
     }
     async.waterfall([
         function (done) {
-            tokens.token_validate(token,function(info){
+            user.token_validate(token,function(info){
                 if(!info)
                     done('token验证失败，无权限更改数据！',null);
                 else
@@ -145,44 +149,62 @@ router.put('/:id', function(req, res, next) {
             });
         },
         function (result, done) {
-            sql = "select id,article_type from template_article where id=$1";
-            let query = client.query(sql, [id], function (err, result) {
-                if (err)
-                    done(err, null);
+            //更新name
+            if(it_services.name!=undefined) {
+                let sql = 'update it_services set name=$1 where idcode=$2';
+                let values = [it_services.name, idcode];
+                let query = client.query(sql, values, function (err, result) {
+                    if (err)
+                        done(err, null);
+                    else{
+                        if(it_services.dependency!=undefined)
+                            done(null, 'ok');
+                        else
+                            done(null, undefined);
+                    }
+                });
+            }
+            else{
+                if(it_services.dependency!=undefined)
+                    done(null, 'ok');
                 else
-                    done(null, result.rows);
-            });
+                    done(null, undefined);
+            }
         },
-        function (rows, done) {
-            if(rows.length==0) {
-                done('根据id未获取更新的记录！', null);
-                return;
+        function (result, done) {
+            if(result==undefined)//没有依赖，直接跳走
+                done(null,undefined);
+            else{//查询信息
+                let sql='select * from it_services where idcode=$1';
+                client.query(sql,[idcode],function(err, result){
+                    if(err)
+                        done(err,null);
+                    else{
+                        var row=result.rows[0];
+                        if(row==undefined)
+                            done('未获取要更新的记录！',null);
+                        else{
+                            let path=row.path;
+                            let dependency=row.dependency;
+                            let sql=util.format("update it_services set dependency=string_to_array(regexp_replace(array_to_string(dependency, ','), '%s', '%s'),',') where path<@'%s'",dependency.join(','),it_services.dependency.join(','),path);
+                            done(null,sql);
+                        }
+                    }
+                });
             }
-            var template_type = rows[0].article_type;
-            if (template_type == undefined) {
-                done('操作文章类型不明确！', null);
-                return;
+        },
+        function (sql, done) {
+            if(sql==undefined)//没有依赖，直接跳走
+                done(null,undefined);
+            else{
+                let query = client.query(sql,function(err, result){
+                    if(err)
+                        done(err,null);
+                    else
+                        done(null,'ok');
+
+                });
             }
-            var template;
-            //根据文章类型，返回需要的sql模板
-            switch (template_type) {
-                case Article_Type.Free:
-                    template = new Template_Free(req.body);
-                    break;
-                case Article_Type.Guide:
-                    template = new Template_Guide(req.body);
-                    break;
-                case Article_Type.Troubleshooting:
-                    template = new Template_Troubleshooting(req.body);
-                    break;
-            }
-            var update_sql = sql_template.updateSQL(template, template_type);
-            let query = client.query(update_sql, [id],function(err,result){
-                if(err)
-                    done(err,null);
-                else
-                    done(err,'ok');
-            });
         }], function (error, result) {
         if (error)
             res.send({status: error});
@@ -191,36 +213,30 @@ router.put('/:id', function(req, res, next) {
         client.end();
     });
 });
-//根据id更新某一个字段
-router.patch('/:id', function(req, res, next) {
-    //获取文章类型
-    var id=req.params.id;
+//根据idcode更新某一个字段
+router.patch('/:idcode', function(req, res, next) {
+    var idcode=req.params.idcode;
     var token=req.body.token;
-    if(id==undefined){
-        res.send({status:'未指定更新id，更新失败！'});
+    if(idcode==undefined){
+        res.send({status:'未指定更新idcode，更新失败！'});
         return;
     }
-    let count=0;
-    for(var key in req.body){
-        if(key!=='token')//忽视token
-            count++;
-    }
-    if(count>1)
-    {
-        res.send({status:'更新字段超过多个，rest权限不够，考虑put请求，更新失败！'});
+    var it_services=new IT_Services(req.body);
+    if(it_services.name==undefined&&it_services.dependency==undefined){
+        res.send({status:'未指定更新字段数据，更新失败！'});
         return;
     }
     var client = new Client(pg_config);
-    try {
+    try{
         client.connect();
     }
     catch(e){
-        res.send({status:e});
+        res.send({status:'数据库连接错误'});
         return;
     }
     async.waterfall([
         function (done) {
-            tokens.token_validate(token,function(info){
+            user.token_validate(token,function(info){
                 if(!info)
                     done('token验证失败，无权限更改数据！',null);
                 else
@@ -228,44 +244,58 @@ router.patch('/:id', function(req, res, next) {
             });
         },
         function (result, done) {
-            sql = "select id,article_type from template_article where id=$1";
-            let query = client.query(sql, [id], function (err, result) {
-                if (err)
-                    done(err, null);
+            //更新name
+            if(it_services.name!=undefined) {
+                let sql = 'update it_services set name=$1 where idcode=$2';
+                let values = [it_services.name, idcode];
+                let query = client.query(sql, values, function (err, result) {
+                    if (err)
+                        done(err, null);
+                    else
+                        done(null, undefined);
+                });
+            }
+            else{
+                if(it_services.dependency!=undefined)
+                    done(null, 'ok');
                 else
-                    done(null, result.rows);
-            });
+                    done(null, undefined);
+            }
         },
-        function (rows, done) {
-            if(rows.length==0) {
-                done('根据id未获取更新的记录！', null);
-                return;
+        function (result, done) {
+            if(result==undefined)//没有依赖，直接跳走
+                done(null,undefined);
+            else{//查询信息
+                let sql='select * from it_services where idcode=$1';
+                client.query(sql,[idcode],function(err, result){
+                    if(err)
+                        done(err,null);
+                    else{
+                        var row=result.rows[0];
+                        if(row==undefined)
+                            done('未获取要更新的记录！',null);
+                        else{
+                            let path=row.path;
+                            let dependency=row.dependency;
+                            let sql=util.format("update it_services set dependency=string_to_array(regexp_replace(array_to_string(dependency, ','), '%s', '%s'),',') where path<@'%s'",dependency.join(','),it_services.dependency.join(','),path);
+                            done(null,sql);
+                        }
+                    }
+                });
             }
-            var template_type = rows[0].article_type;
-            if (template_type == undefined) {
-                done('操作文章类型不明确！', null);
-                return;
+        },
+        function (sql, done) {
+            if(sql==undefined)//没有依赖，直接跳走
+                done(null,undefined);
+            else{
+                let query = client.query(sql,function(err, result){
+                    if(err)
+                        done(err,null);
+                    else
+                        done(null,'ok');
+
+                });
             }
-            var template;
-            //根据文章类型，返回需要的sql模板
-            switch (template_type) {
-                case Article_Type.Free:
-                    template = new Template_Free(req.body);
-                    break;
-                case Article_Type.Guide:
-                    template = new Template_Guide(req.body);
-                    break;
-                case Article_Type.Troubleshooting:
-                    template = new Template_Troubleshooting(req.body);
-                    break;
-            }
-            var update_sql = sql_template.updateSQL(template, template_type);
-            let query = client.query(update_sql, [id],function(err,result){
-                if(err)
-                    done(err,null);
-                else
-                    done(err,'ok');
-            });
         }], function (error, result) {
         if (error)
             res.send({status: error});
@@ -274,73 +304,43 @@ router.patch('/:id', function(req, res, next) {
         client.end();
     });
 });
-//根据id查询
-router.get('/:id', function(req, res, next) {
-    var id=req.params.id;
-    if(id==undefined){
-        res.send({status:'未指定查询id，查询失败！'});
-        return;
-    }
+//根据idcode查询，只查询该it_services子服务
+router.get('/:idcode', function(req, res, next) {
+    var idcode=req.params.idcode;
     var client = new Client(pg_config);
     try {
         client.connect();
     }
     catch(e){
-        res.send({status:e});
+        res.send({status:"查询服务器发生错误！"});
         return;
     }
-    async.waterfall([
-            function (done) {
-                sql = "select id,article_type from template_article where id=$1";
-                query = client.query(sql, [id],function(err,result){
-                    if(err)
-                        done(err,null);
-                    else
-                        done(null,result.rows);
-                });
-            },
-            function (rows, done) {
-                if(rows.length==0) {
-                    done('根据id未获取更新的记录！', null);
-                    return;
-                }
-                var template_type = rows[0].article_type;
-                if (template_type == undefined) {
-                    done('操作文章类型不明确！', null);
-                    return;
-                }
-                let sql = util.format("select * from %s where id=$1", template_type);
-                query = client.query(sql, [id],function(err,result){
-                    if(err)
-                        done(err,null);
-                    else
-                        done(null,result.rows);
-                });
-            }], function (error, result) {
-            if (error)
-                res.send({status:error});
-            else
-                res.send({status:result});
-            client.end();
-        }
-    )
+    let sql;//查询某个idcode下的子服务列表
+    sql='select a.* from it_services a,(select nlevel(path) as level,path from it_services where idcode=$1) b where a.path<@b.path and nlevel(a.path)=(b.level+1)';
+    var query = client.query(sql, [idcode],function(err,result){
+        if(err)
+            res.send({status:'查询发生错误！'});
+        else
+            res.send({status:result.rows});
+    });
 });
-//无条件查询，支持排序，分页
+//分组查询
 router.get('/', function(req, res, next) {
-    let querys = req.query;
+    var client = new Client(pg_config);
     try {
-        var client = new Client(pg_config);
         client.connect();
-        let sql = sql_template.querySQL(querys, 'template_article');
-        let query = client.query(sql,function(err, result) {
-            res.send({status: 'ok', data: result.rows});
-            client.end();
-        });
     }
-    catch(e) {
-        console.log(e);
-        res.send({status: e});
+    catch(e){
+        res.send({status:"查询服务器发生错误！"});
+        return;
     }
+    let sql='select * from it_services where nlevel(path)=1';
+    var query = client.query(sql,function(err,result){
+        if(err)
+            res.send({status:'查询发生错误！'});
+        else
+            res.send({status:result.rows});
+    });
 });
 var uuid=function() {
     var s = [];
