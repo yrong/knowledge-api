90d726cae9c3d4b748e453d6636ead4879dd13a2
yrong <yrong1997@gmail.com>
2017-01-16 19:13:09 +0800

    按新旧接口区分kb-api测试用例(1小时)
    按新旧接口区分kb-api测试用例

--- test/postman.js
+++ test/postman.js
@@ -2,10 +2,19 @@ var newman = require('newman'); // require newman in your project
 
 // call newman.run to pass `options` object and wait for callback
 newman.run({
-    collection: require('./knowledge.postman_collection.json'),
-    environment: require('./knowledge.postman_environment.json'),
+    collection: require('./kb-api.postman_collection.json'),
+    environment: require('./kb.postman_environment.json'),
     reporters: 'cli'
 }, function (err) {
     if (err) { throw err; }
-    console.log('collection run complete!');
+    console.log('kb-api run complete!');
+});
+
+newman.run({
+    collection: require('./kb-api-v1.postman_collection.json'),
+    environment: require('./kb.postman_environment.json'),
+    reporters: 'cli'
+}, function (err) {
+    if (err) { throw err; }
+    console.log('kb-api v1 run complete!');
 });
