cc61baf30c1a8b84283d492baaf93d858a2e3cb2
yrong <yrong1997@gmail.com>
2017-06-07 10:28:01 +0800

    check cors login(0.5h)

--- middleware/check_token.js
+++ middleware/check_token.js
@@ -8,12 +8,16 @@ module.exports = function checktoken(token_check_url,token_name) {
             || (req.body && req.body[token_name])
             || req.cookies[token_name]
         let options = {uri:token_check_url,method:'POST',json:true,body:{token:token}}
-        rp(options).then((result)=>{
-            _.assign(req,result.data)
-            next();
-        }).catch(error=>{
-            res.status(401)
-            next(error)
-        })
+        if(req.url === '/' || req.url.indexOf('.html') >= 0){
+            next()
+        }else{
+            rp(options).then((result)=>{
+                _.assign(req,result.data)
+                next();
+            }).catch(error=>{
+                res.status(401)
+                next(error)
+            })
+        }
     }
 }
