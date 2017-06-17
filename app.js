7c1e8567d95d8a72ecaf2f349524cb9e1a9dee74
yrong <yrong1997@gmail.com>
2017-06-17 15:00:07 +0800

    kb集成nc上传组件（2h)
    kb集成上传组件(参考demo页面使用方式)

--- app.js
+++ app.js
@@ -1,33 +1,43 @@
+const _ = require('lodash')
+const config = require('config')
+const logger = require('log4js_wrapper')
 /**
  * init logger
  */
-const logger = require('log4js_wrapper')
-logger.initialize()
 
-/**
- * init middlewares
- */
-const config = require('config');
+logger.initialize(config.get('logger'))
+
+
 const Koa = require('koa')
 const convert = require('koa-convert')
 const cors = require('kcors')
 const Static = require('koa-static')
 const mount = require('koa-mount')
 const bodyParser = require('koa-bodyparser')
+const responseWrapper = require('koa-response-wrapper')
+const check_token = require('koa-token-checker')
+const dbHelper = require('./helper/db_helper')
+const models = require('./models')
+const router = require('./routes')
+
+
+/**
+ * init middlewares
+ */
+
 const app = new Koa();
 app.use(cors({ credentials: true }))
 app.use(bodyParser())
 app.use(mount("/", convert(Static(__dirname + '/public'))))
-const responseWrapper = require('koa-response-wrapper')
 app.use(responseWrapper())
-const check_token = require('koa-token-checker')
-app.use(check_token())
-
+app.use(check_token(config.get('auth')))
+const file_uploader = require('koa-file-upload-fork')
+for(let option of _.values(config.get('upload'))){
+    app.use(mount(option.url,file_uploader(option).handler))
+}
 /**
  * init orm
  */
-const dbHelper = require('./helper/db_helper')
-const models = require('./models')
 let cleandb = async ()=>{
     await dbHelper.pool.query(`DROP TABLE IF EXISTS "Articles"`)
     await dbHelper.pool.query(`DROP TABLE IF EXISTS "Discussions"`)
@@ -51,7 +61,6 @@ if(process.env.RebuildSchema){
 /**
  * init routes
  */
-const router = require('./routes')
 app.use(router.routes())
 
 /**
